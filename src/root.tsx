a6fe2c895a97a346f32c33a46ef83f4d9694f3bc
Arseniy Oguzov <oguzov.a@n-t.io>
2023-08-17 16:11:42 +0400

    small improvements

--- src/root.tsx
+++ src/root.tsx
@@ -20,6 +20,7 @@ export default function Root() {
                 <Title>psy667.dev</Title>
                 <Meta charset="utf-8" />
                 <Meta name="viewport" content="width=device-width, initial-scale=1" />
+                <Meta name="theme-color" content="#171717" />
             </Head>
             <Body>
                 <Suspense>
